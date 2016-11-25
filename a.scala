2ed7d0752f0ffa647fe4e83383fcb5c890a61f0d
SunJc <331631622@qq.com>
2016-11-25 23:29:07 +0800

    Update a.scala

--- a.scala
+++ a.scala
@@ -1,21 +1,16 @@
-import org.apache.spark.SparkContext
-import org.apache.spark._
+//import org.apache.spark.SparkContext
+//import org.apache.spark._
 import org.apache.spark.graphx._
 // To make some of the examples work we will also need RDD
-import org.apache.spark.rdd.RDD
+//import org.apache.spark.rdd.RDD
 // Assume the SparkContext has already been constructed
-val sc: SparkContext
+
 // Load the edges as a graph
-val graph = GraphLoader.edgeListFile(sc, "graphx/data/collaboration.txt")
+val graph = GraphLoader.edgeListFile(sc, "data/graphx/followers.txt")
 // Run PageRank
 val ranks = graph.pageRank(0.0001).vertices
 // Join the ranks with the usernames
-val persons = sc.textFile("graphx/data/persons.txt").map { line =>
-  val fields = line.split(",")
-  (fields(0).toLong, fields(1))
-}
-val ranksByPersonname = users.join(ranks).map {
-  case (id, (personName, rank)) => (personName, rank)
-}
+val persons = sc.textFile("data/graphx/users.txt").map { line => val fields = line.split(",") ;(fields(0).toLong, fields(1)) }
+val ranksByPersonname = persons.join(ranks).map { case (id, (personName, rank)) => (personName, rank) }
 // Print the result
-println(ranksByUsername.collect().mkString("\n"))
+println(ranksByPersonname.collect().mkString("\n"))
