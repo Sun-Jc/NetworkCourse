a2c78b6a542e7f01b6c20874be89e51159ded329
SunJc <331631622@qq.com>
2016-11-25 23:36:07 +0800

    Update

--- a.scala
+++ a.scala
@@ -1,16 +1,25 @@
 //import org.apache.spark.SparkContext
 //import org.apache.spark._
 import org.apache.spark.graphx._
+import java.io._
+
 // To make some of the examples work we will also need RDD
 //import org.apache.spark.rdd.RDD
 // Assume the SparkContext has already been constructed
 
+val edgeFile = "data/graphx/followers.txt"
+val nodeFile = "data/graphx/users.txt"
+
 // Load the edges as a graph
-val graph = GraphLoader.edgeListFile(sc, "data/graphx/followers.txt")
+val graph = GraphLoader.edgeListFile(sc, edgeFile)
 // Run PageRank
 val ranks = graph.pageRank(0.0001).vertices
 // Join the ranks with the usernames
-val persons = sc.textFile("data/graphx/users.txt").map { line => val fields = line.split(",") ;(fields(0).toLong, fields(1)) }
+val persons = sc.textFile(nodeFile).map { line => val fields = line.split(",") ;(fields(0).toLong, fields(1)) }
 val ranksByPersonname = persons.join(ranks).map { case (id, (personName, rank)) => (personName, rank) }
 // Print the result
-println(ranksByPersonname.collect().mkString("\n"))
+//println(ranksByPersonname.collect().mkString("\n"))
+val file = new File("data/graphx/ranks.txt")
+val bw = new BufferedWriter(new FileWriter(file))
+bw.write(ranksByPersonname.collect().mkString("\n"))
+bw.close()
