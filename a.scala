6b88f8f17028bc2dcf14c98e9da35df2e5a5b68a
SunJc <331631622@qq.com>
2016-11-25 22:05:01 +0800

    Update a.scala

--- a.scala
+++ a.scala
@@ -1,3 +1,8 @@
+import org.apache.spark.SparkContext
+import org.apache.spark._
+import org.apache.spark.graphx._
+// To make some of the examples work we will also need RDD
+import org.apache.spark.rdd.RDD
 // Assume the SparkContext has already been constructed
 val sc: SparkContext
 // Load the edges as a graph
